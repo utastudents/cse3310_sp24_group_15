94c66d3ffb671536c029cb791bd4482ab3aed7cd
jnguyen5151 <40365611+jnguyen5151@users.noreply.github.com>
2024-03-12 16:58:56 -0500

    added pom.xml dependencies
    added required dependencies for websockets, .net, and com.google to the pomxml files

--- twsg/src/main/java/uta/cse3310/App.java
+++ twsg/src/main/java/uta/cse3310/App.java
@@ -1,9 +1,27 @@
 package uta.cse3310;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+import java.time.Instant;
+import java.time.Duration;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class App 
 {
     public static void main( String[] args )
