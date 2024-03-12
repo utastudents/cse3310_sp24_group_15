0d8eef9d957497c184fd91b0c496ffb5360ccece
jnguyen5151 <40365611+jnguyen5151@users.noreply.github.com>
2024-03-12 16:46:52 -0500

    replaces old structure with newly created using maven
    Replaced old structure I made after finding out I could just use "Sample Maven Archetype Quickstart"

---
+++ twsg/src/main/java/uta/cse3310/App.java
@@ -0,0 +1,13 @@
+package uta.cse3310;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
